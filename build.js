ce728c53369c6c94b2fad9fe6bbaf3b152b51398
Shreshth Khilani <shreshth.khilani@gmail.com>
2026-01-25 14:21:19 -0500

    Add a favicon

--- build.js
+++ build.js
@@ -91,6 +91,7 @@ const html = `<!DOCTYPE html>
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" type="image/png" href="favicon.png">
 <title>filmstruck</title>
 <style>
 /* Poster hover effect */
